d63fb6ee767514429fe9fb145f04b187d2aae168
Marselina-you <marselina88@list.ru>
2020-11-12 14:15:21 +0300

    файл регистрации

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	// $('body').hide()
+	$('.header-box1__padding a').click(function(){
+    var url = $(this).attr('href');
+    $('.registration').load(url + ' .block-registration-enter');
+    return false;
+    });
 })
