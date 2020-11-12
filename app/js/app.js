1c9450bf5ff973d0d9fa2451d2200d254578c604
Marselina-you <marselina88@list.ru>
2020-11-12 15:48:09 +0300

    работающий сценарий Ajax

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('.header-box1__padding a').click(function(){
+	$('.header-box1__href').click(function(){
     var url = $(this).attr('href');
-    $('.registration').load(url + ' .block-registration-enter');
+    $('.registration').load(url + ' .img');
     return false;
     });
 })
